2be9937288714b5dcc460b7ce21033edc2ed1ba2
Guga <contato.gustavobarbosa@outlook.com>
2024-03-02 01:33:41 -0300

    Fix text max-width from LastCall component

--- src/components/LastCall/index.tsx
+++ src/components/LastCall/index.tsx
@@ -9,11 +9,11 @@ const LastCall: React.FC = () => {
   return (
     <Container>
       <div className="w-full h-last-call px-4 py-8 rounded-2xl flex flex-col justify-center items-center gap-6 text-center bg-last-call">
-        <h1 className="w-full w-max-last-call-title font-poppins font-semibold text-4xl @laptop:text-5xl">
+        <h1 className="w-full max-w-max-last-call-title font-poppins font-semibold text-4xl @laptop:text-5xl">
           Abra as Portas para a Próxima Revolução Digital!
         </h1>
-        <p className="w-full w-max-last-call-subtitle font-inter @laptop:text-xl">
-          Descubra, Adquira e Possua os NFTs Mais Valiosos do Universo Digital. Sua Oportunidade, Sua História.
+        <p className="w-full max-w-last-call-subtitle font-inter @laptop:text-xl">
+          Descubra, adquira e possua os NFTs mais valiosos do universo digital. Sua Oportunidade, sua história.
         </p>
         <div className="flex flex-col items-center gap-4 @laptop:flex-row">
           <button>
